6bce98ade1870c33996c07928cd662688a36d4e7
Luka <Lukagrubesa26@gmail.com>
2022-03-15 14:11:07 +0100

    vid3 pt1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div class="row">
+ <div class="col-2"></div>
+ <div class="col-7">
+  <instagram-card v-for="card in cards" :key="card" :info="card" />
+
+ </div>
+ <div class="col-3">
+ ... ovdje ide desni stupac!
+ </div>
+ </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import InstagramCard from '@/components/InstagramCard.vue'
+
+let cards = [];
+
+cards = [
+  "https://picsum.photos/id/1/400/400",
+  "https://picsum.photos/id/3/400/400",
+  "https://picsum.photos/id/5/400/400",
+]
+
 
 export default {
-  name: 'HomeView',
+  props: ["info"],
+  name: 'home',
+  data: function() {
+    return {
+        cards: cards,
+    }
+  },
   components: {
-    HelloWorld
+    InstagramCard
   }
 }
 </script>
