5eaad8f3e267c084af67b5d8e646fc8e6bbcdaaa
Luka <Lukagrubesa26@gmail.com>
2022-03-28 12:26:04 +0200

    vid3+dz

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,14 @@
  <div class="row">
  <div class="col-2"></div>
  <div class="col-7">
-  <instagram-card v-for="card in cards" :key="card" :info="card" />
+  <instagram-card v-for="card in filteredCards" :key="card.url" :info="card" />
 
  </div>
  <div class="col-3">
- ... ovdje ide desni stupac!
+ <input v-model='ime' placeholder="Upišite ime">
+ <input v-model='prezime' placeholder="Upišite prezime">
+ 
+ <p>{{ime}} {{prezime}}</p>
  </div>
  </div>
 </template>
@@ -14,13 +17,23 @@
 <script>
 // @ is an alias to /src
 import InstagramCard from '@/components/InstagramCard.vue'
+import store from "@/store";
 
 let cards = [];
+let ime= [];
+let prezime = [];
+
 
 cards = [
-  "https://picsum.photos/id/1/400/400",
-  "https://picsum.photos/id/3/400/400",
-  "https://picsum.photos/id/5/400/400",
+  {url  : 'https://picsum.photos/id/1/400/400',
+  "description": "laptop",
+  time: 'few minutes ago...'},
+  {url  : 'https://picsum.photos/id/5/400/400',
+  "description": "laptop",
+  time: 'few minutes ago...'},
+  {url  : 'https://picsum.photos/id/10/400/400',
+  "description": "laptop",
+  time: 'few minutes ago...'},
 ]
 
 
@@ -29,11 +42,39 @@ export default {
   name: 'home',
   data: function() {
     return {
-        cards: cards,
+        cards,
+        store,
+        ime: "",
+        prezime: "",
+        imePrint:"",
+        prezimePrint:"",
+      
+       
     }
   },
+  computed: {
+    
+     filteredCards() {
+       let termin = this.store.searchTerm;
+
+       return this.cards.filter(card => card.description.includes(termin));
+       
+
+    
+     },
+    
+    
+  },
+
+  
+  imePrezime() {
+      this.imePrint=this.ime;
+      this.prezimePrint=this.prezime;
+    },
+
+  
   components: {
-    InstagramCard
+    InstagramCard,
   }
 }
 </script>
